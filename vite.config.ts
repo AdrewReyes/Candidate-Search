00234af4a8480d69533c17915935cafa01698ff7
AdrewReyes <andrewreyes.m@gmail.com>
2025-06-09 16:50:01 -0700

    render vite update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir: './env',
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: ['candidate-search-st9j.onrender.com'],
+  },
 });
